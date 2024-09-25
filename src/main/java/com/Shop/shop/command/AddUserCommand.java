b477fbe4749909ab1abaa2b0481581c90fc976ff
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2024-09-25 19:29:53 +0200

    Create classes User and AddUserCommand
    with required fields and validation: id, name, lastname, username, email, phoneNumber, password.

---
+++ src/main/java/com/Shop/shop/command/AddUserCommand.java
@@ -0,0 +1,29 @@
+package com.Shop.shop.command;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AddUserCommand {
+    private Long id;
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+    @NotBlank(message = "LastName cannot be empty")
+    private String lastname;
+    @NotBlank(message = "User cannot be empty")
+    private String username;
+    @Email(message = "Email doesn't meet the requirements")
+    @NotNull(message = "Email cannot be empty")
+    private String email;
+    @NotNull(message = "Phone number cannot be empty")
+    @Size(min = 9, max = 9, message = "Password must be exactly 8 characters long")
+    @Pattern(regexp = "^\\d{9}$", message = "Invalid phone number")
+    private String phoneNumber;
+    @NotNull(message = "Password cannot be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+}
---
+++ src/main/java/com/Shop/shop/model/User.java
@@ -0,0 +1,27 @@
+package com.Shop.shop.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String lastname;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+    @Column(nullable = false)
+    private String password;
+}
