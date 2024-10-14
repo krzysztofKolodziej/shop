9a3a6b1203ab93d9d25f8fa81c48496b08e12ac0
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2024-10-14 10:18:51 +0200

    Add class KafkaTopicConfig
    to crate bean admin and createOrderTopic.

---
+++ src/main/java/com/Shop/shop/config/kafkaConfig/KafkaTopicConfig.java
@@ -0,0 +1,28 @@
+package com.Shop.shop.config.kafkaConfig;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics createOrderTopic() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("order-events")
+                        .build());
+    }
+}
